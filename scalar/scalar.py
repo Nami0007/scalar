26e0ed3c23bde856cb7032e5ebf83f1bf84b7bce
Nami0007 <rohithrao444@gmail.com>
2022-11-14 14:26:37 +0530

    change1

---
+++ scalar/scalar.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  1 03:03:27 2005
+
+@author: cse
+"""
+
+number_of_units=50
+mass_of_sum=1.989e30
+length_great_wall_of_china=21196
+print(number_of_units)
+print(mass_of_sum)
+print(length_great_wall_of_china)
